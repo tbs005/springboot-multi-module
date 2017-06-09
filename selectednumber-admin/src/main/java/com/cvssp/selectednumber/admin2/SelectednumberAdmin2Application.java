7900c856c6552c518eb72da92a365eaf2b3d943d
tbs005 <tbs005@qq.com>
2017-06-10 00:41:53 +0800

    springboot

---
+++ selectednumber-admin/src/main/java/com/cvssp/selectednumber/admin2/SelectednumberAdmin2Application.java
@@ -0,0 +1,12 @@
+package com.cvssp.selectednumber.admin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectednumberAdmin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectednumberAdmin2Application.class, args);
+	}
+}
---
+++ selectednumber-web/src/main/java/com/cvssp/selectednumber/web/SelectednumberWebApplication.java
@@ -0,0 +1,12 @@
+package com.cvssp.selectednumber.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectednumberWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectednumberWebApplication.class, args);
+	}
+}
