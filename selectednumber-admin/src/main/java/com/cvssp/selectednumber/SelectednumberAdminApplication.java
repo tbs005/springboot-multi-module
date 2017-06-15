adde0444b4cc1cb9bdfc710a93edbefc217cf6e2
tbs005 <tbs005@qq.com>
2017-06-16 01:05:16 +0800

    配置修改

--- selectednumber-admin/src/main/java/com/cvssp/selectednumber/SelectednumberAdminApplication.java
+++ selectednumber-admin/src/main/java/com/cvssp/selectednumber/SelectednumberAdminApplication.java
@@ -2,12 +2,9 @@ package com.cvssp.selectednumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import com.cvssp.selectednumber.service.ServiceConfiguration;
 
 @SpringBootApplication
-@Import(ServiceConfiguration.class)
+//@Import(ServiceConfiguration.class)
 public class SelectednumberAdminApplication {
 
 	public static void main(String[] args) {
